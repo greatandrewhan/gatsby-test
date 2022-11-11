1b69c6ea6148700b2765fdf080894be86bdd387f
Sung Kil Han <90734610+greatandrewhan@users.noreply.github.com>
2022-11-10 22:40:37 -0500

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,21 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `gatsby-test`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // If you use VSCode you can also use the GraphQL plugin
+  // Learn more at: https://gatsby.dev/graphql-typegen
+  graphqlTypegen: true,
+  plugins: [{
+    resolve: 'gatsby-source-contentful',
+    options: {
+      "accessToken": "\u0016\u0016Eqd-0nYqoRPryo9AC_KaAE-mCUcQYE9hht6cQ-VJcY4",
+      "spaceId": ""
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp"]
+};
+
+export default config;
